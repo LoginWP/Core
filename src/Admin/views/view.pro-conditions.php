ab33ed7bb7a59e2ab15a2d1b73295a333a6a6a78
Collins Agbonghama <collizo4sky@gmail.com>
2022-09-06 17:55:33 +0100

    yolo

--- src/Admin/views/view.pro-conditions.php
+++ src/Admin/views/view.pro-conditions.php
@@ -10,6 +10,9 @@ $pro_conditions = [
         esc_html__('Active User Subscription', 'peters-login-redirect'),
         esc_html__('Active Membership Plan', 'peters-login-redirect')
     ],
+    'ProfilePress'         => [
+        esc_html__('Subscribed Membership Plan', 'peters-login-redirect')
+    ],
     'MemberPress'          => [
         esc_html__('Subscribed Membership', 'peters-login-redirect')
     ],
@@ -20,13 +23,13 @@ $pro_conditions = [
         esc_html__('Enrolled Course', 'peters-login-redirect'),
         esc_html__('Enrolled Membership', 'peters-login-redirect')
     ],
-    'TutorLMS'            => [
+    'TutorLMS'             => [
         esc_html__('Enrolled Course', 'peters-login-redirect')
     ],
-    'WPML'            => [
+    'WPML'                 => [
         esc_html__('Website Current Language', 'peters-login-redirect')
     ],
-    'Polylang'            => [
+    'Polylang'             => [
         esc_html__('Website Current Language', 'peters-login-redirect')
     ]
 ];
